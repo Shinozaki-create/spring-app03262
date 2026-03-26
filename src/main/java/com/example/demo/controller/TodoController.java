386e720bbdb249165c20b4790bcd4fa4f781cd5a
DESKTOP-S3L5LC2\academia <202512_aw_shinozaki@x-ide.sakura.ne.jp>
2026-03-26 16:26:43 +0900

    sec3-1 @RequestParamでフォームデータを受け取る

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/todo")
@@ -24,11 +26,18 @@ public class TodoController {
 
     @GetMapping("/new")
     public String showNewForm() {
-        return "todo/new";
+        return "todo/form";
     }
 
-    @GetMapping("/confirm")
-    public String showConfirm() {
+    @PostMapping("/confirm")
+    public String confirm(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("priority") String priority,
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("priority", priority);
         return "todo/confirm";
     }
 
